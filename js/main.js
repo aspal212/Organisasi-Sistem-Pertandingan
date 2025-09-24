2dca6b0dbbe89647d8765ca5a5f05d0b3b6e51f8
aspal212 <cusmin212@gmail.com>
2025-09-24 16:00:30 +0000

    add

--- js/main.js
+++ js/main.js
@@ -14,10 +14,11 @@
 
   // Deskripsi untuk setiap sistem
   const descriptions = {
-    "Sistem Gugur": "Sistem Gugur: Peserta kalah langsung keluar dari turnamen.",
-    "Gugur Ganda": "Gugur Ganda: Peserta harus kalah dua kali sebelum keluar.",
-    "Setengah Kompetisi": "Setengah Kompetisi: Setiap peserta bertanding melawan setengah peserta lain.",
-    "Kompetisi Penuh": "Kompetisi Penuh: Setiap peserta bertanding dengan semua peserta lain."
+    "Sistem Gugur Tunggal": "Sistem Gugur Tunggal: Peserta kalah langsung keluar dari turnamen.",
+    "Sistem Gugur Ganda": "Sistem Gugur Ganda: Peserta harus kalah dua kali sebelum keluar.",
+    "Sistem Gugur Swiss": "Sistem Gugur Swiss: Peserta bertanding dengan lawan yang memiliki skor sama, hingga jumlah ronde tercapai.",
+    "Sistem Setengah Kompetisi": "Setengah Kompetisi: Setiap peserta bertanding melawan setengah peserta lain.",
+    "Sistem Kompetisi Penuh": "Kompetisi Penuh: Setiap peserta bertanding dengan semua peserta lain."
   };
 
   // Buka modal
@@ -61,16 +62,19 @@
     setTimeout(function() {
       let targetURL = "";
       switch(format) {
-        case "Sistem Gugur":
+        case "Sistem Gugur Tunggal":
           targetURL = "sistem/sistem_gugur.html";
           break;
-        case "Gugur Ganda":
+        case "Sistem Gugur Ganda":
           targetURL = "sistem/sistem_gugur_ganda.html";
           break;
-        case "Setengah Kompetisi":
+        case "Sistem Gugur Swiss":
+          targetURL = "sistem/sistem_gugur_swiss.html";
+          break;
+        case "Sistem Setengah Kompetisi":
           targetURL = "sistem/sistem_setengah_kompetisi.html";
           break;
-        case "Kompetisi Penuh":
+        case "Sistem Kompetisi Penuh":
           targetURL = "sistem/sistem_kompetisi_penuh.html";
           break;
         default:
